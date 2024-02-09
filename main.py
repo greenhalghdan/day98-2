1443740b177fe59b2ba53be10775f383e680ff05
greenhalghdan <49040634+greenhalghdan@users.noreply.github.com>
2024-02-09 19:57:35 +0000

    remove redundent code

--- main.py
+++ main.py
@@ -6,13 +6,9 @@ host = {
     '192.168.1.240': 0,
     '192.168.1.250': 0
 }
-failure = 0
 
 running = True
 while running:
-    result = troubleshooting.checkhostonline(host)
-    if result == 0:
-        failure += 1
-    # if failure > 3:
+    troubleshooting.checkhostonline(host)
 
     time.sleep(5)
